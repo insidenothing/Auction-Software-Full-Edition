c18c851fa229e7372a304cd881cb9d4042f4d27e
Patrick McGuire <insidenothing@gmail.com>
2010-10-19 21:35:40 -0400

    using JSON to pull live information from the server

--- src/com/example/helloandroid/HelloAndroid.java
+++ src/com/example/helloandroid/HelloAndroid.java
@@ -1,15 +1,88 @@
 package com.example.helloandroid;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class HelloAndroid extends Activity {
    /** Called when the activity is first created. */
-   @Override
+   @SuppressWarnings("null")
+@Override
    public void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       TextView tv = new TextView(this);
-       tv.setText("Hack the Planet, again");
-       setContentView(tv);
+	   
+	   
+	   
+	   
+	   
+	   String result = "";
+	   //the year data to send
+	   ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+	   nameValuePairs.add(new BasicNameValuePair("year","1980"));
+	   //http post
+	   try{
+	 		  HttpClient httpclient = new DefaultHttpClient();
+	 		  HttpPost httppost = new HttpPost("http://209.251.35.217/getAllPeopleBornAfter.php");
+	 		  httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+	 		  HttpResponse response = httpclient.execute(httppost);
+	 		  HttpEntity entity = response.getEntity();
+	 		  @SuppressWarnings("unused")
+			InputStream is = entity.getContent();
+	   }catch(Exception e){
+	 		  Log.e("log_tag", "Error in http connection "+e.toString());
+	   }
+	   //convert response to string
+	   try{
+	 		  InputStream is = null;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+	 		  StringBuilder sb = new StringBuilder();
+	 		  String line = null;
+	 		  while ((line = reader.readLine()) != null) {
+	 				  sb.append(line + "\n");
+	 		  }
+	 		  is.close();
+	 		  result=sb.toString();
+	   }catch(Exception e){
+	 		  Log.e("log_tag", "Error converting result "+e.toString());
+	   }
+	   //parse json data
+	   try{
+	 		  JSONArray jArray = new JSONArray(result);
+	 		  for(int i=0;i<jArray.length();i++){
+	 				  JSONObject json_data = jArray.getJSONObject(i);
+	 				  Log.i("log_tag","id: "+json_data.getInt("id")+
+	 						  ", name: "+json_data.getString("name")+
+	 						  ", sex: "+json_data.getInt("sex")+
+	 						  ", birthyear: "+json_data.getInt("birthyear")
+	 				  );
+	 		  }
+	   }catch(JSONException e){
+	 		  Log.e("log_tag", "Error parsing data "+e.toString());
+	   }
+	   
+	   
+	   
+	   
+		  super.onCreate(savedInstanceState);
+		      TextView tv = new TextView(this);
+		      tv.setText("Pull data from MySQL...");
+		      setContentView(tv); 
+   
    }
 }

b169211a970ec8ba9ba83c46a2fdf08e9038d07c
Patrick McGuire <insidenothing@gmail.com>
2010-10-23 15:02:09 -0400

    forgot to add before last commit

---
+++ src/com/example/helloandroid/BuildingBlock.java
@@ -0,0 +1,12 @@
+package com.example.helloandroid;
+
+import android.util.Log;
+
+public class BuildingBlock {
+	public BuildingBlock(HelloAndroid helloAndroid) {
+		// TODO Auto-generated constructor stub
+		Log.i("patrick-code","Ready to expand.");
+	}
+
+
+}

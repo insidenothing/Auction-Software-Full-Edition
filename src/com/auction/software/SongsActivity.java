cf6a4eb2b986f5224a2f9ff1b7fad76b1e8dc410
Patrick McGuire <insidenothing@gmail.com>
2010-10-28 13:50:20 -0400

    v1.0

--- src/com/auction/software/SongsActivity.java
+++ src/com/auction/software/SongsActivity.java
@@ -1,48 +1,17 @@
 package com.auction.software;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-
-public class SongsActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i("patrick-code", "auction search");
+import android.widget.TextView;
 
 
-        //setContentView(R.layout.search);
-
-        
-       
-
-
-       
-
-          // public static void main (String[] args) {
-
-              //  prompt the user to enter their name
-              System.out.print("Enter your name: ");
-
-              //  open up standard input
-              BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-              String userName = null;
-
-              //  read the username from the command-line; need to use try/catch with the
-              //  readLine() method
-              try {
-                 userName = br.readLine();
-              } catch (IOException ioe) {
-                 System.out.println("IO error trying to read your name!");
-                 System.exit(1);
-              }
-
-              System.out.println("Thanks for the name, " + userName);
-
-           }
-
-        }  // end of ReadString class
+	public class SongsActivity extends Activity {
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        Log.i("patrick-code", "full edition holder");
+	        TextView textview = new TextView(this);
+	        textview.setText("This will be available in the full edition.");
+	        setContentView(textview);
+	    }
+	}

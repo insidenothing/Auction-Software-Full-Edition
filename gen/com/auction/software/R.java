9582c57649968b1d001dc548104f817bb559ae2d
Patrick McGuire <insidenothing@gmail.com>
2010-10-31 09:05:52 -0400

    updated icons inside app

--- gen/com/auction/software/R.java
+++ gen/com/auction/software/R.java
@@ -11,12 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_tab_albums=0x7f020000;
-        public static final int ic_tab_artists=0x7f020001;
-        public static final int ic_tab_artists_grey=0x7f020002;
-        public static final int ic_tab_artists_white=0x7f020003;
+        public static final int around_me_color=0x7f020000;
+        public static final int around_me_grey=0x7f020001;
+        public static final int ic_tab_albums=0x7f020002;
+        public static final int ic_tab_artists=0x7f020003;
         public static final int ic_tab_songs=0x7f020004;
         public static final int icon=0x7f020005;
+        public static final int schedule_list_color=0x7f020006;
+        public static final int schedule_list_grey=0x7f020007;
+        public static final int search_list_color=0x7f020008;
+        public static final int search_list_grey=0x7f020009;
     }
     public static final class id {
         public static final int dateDisplay=0x7f050001;
